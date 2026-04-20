3672e2d6b114fe94270e233d5a96311d2dad342a
skubavadim1981-creator <skubavadim1981@gmail.com>
2026-04-20 22:57:21 +0300

    Add annotation (Extension) for databao tests

--- src/test/java/com/example/jetbrainstest/tests/DatabaoTest.java
+++ src/test/java/com/example/jetbrainstest/tests/DatabaoTest.java
@@ -1,14 +1,17 @@
 package com.example.jetbrainstest.tests;
 
+import com.example.jetbrainstest.MyExtension;
 import com.example.jetbrainstest.pages.DatabaoPage;
 import com.example.jetbrainstest.pages.FleetPage;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(MyExtension.class)
 public class DatabaoTest extends BaseTest {
 
     private DatabaoPage DatabaoPage;
@@ -31,7 +34,7 @@ public class DatabaoTest extends BaseTest {
     @Test
     @DisplayName("Проверка, что форма бронирования презентации видима")
     public void checkIfDistributedButtonIsClickable(){
-        DatabaoPage.clickBookADemoButton();
+        //DatabaoPage.clickBookADemoButton();
         assertTrue(DatabaoPage.checkIfDemoRequestFormIsDisplayed(), "Форма бронирования не видима");
     }
 }
